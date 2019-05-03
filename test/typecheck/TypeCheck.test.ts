8dce321d7bb7e6b52493c81e174e6dd1681e41c8
Rosco Kalis <roscokalis@gmail.com>
2019-05-03 19:09:53 +0900

    Add simple tests for type checking
    - Add a few tests for invalid parameter type errors
    - Still many needs more tests and for different errors

---
+++ test/typecheck/TypeCheck.test.ts
@@ -0,0 +1,60 @@
+/*   TypeCheck.test.ts
+ *
+ * - This file is used to test the functioning of the symbol table.
+ * - It has three different test categories: success, undefined and redefinition.
+ */
+
+import { ANTLRInputStream, CommonTokenStream } from 'antlr4ts';
+import { assert } from 'chai';
+import * as path from 'path';
+import { CashScriptParser } from '../../src/grammar/CashScriptParser';
+import { CashScriptLexer } from '../../src/grammar/CashScriptLexer';
+import { readCashFiles } from '../test-util';
+import SymbolTableTraversal from '../../src/traversals/SymbolTableTraversal';
+import AstBuilder from '../../src/ast/AstBuilder';
+import { SourceFileNode, Node } from '../../src/ast/AST';
+import { UndefinedReferenceError, RedefinitionError, InvalidParameterTypeError } from '../../src/Errors';
+import TypeCheckTraversal from '../../src/traversals/TypeCheckTraversal';
+
+interface TestSetup {
+  ast: Node,
+  traversal: TypeCheckTraversal,
+}
+
+function setup(input: string): TestSetup {
+  const inputStream = new ANTLRInputStream(input);
+  const lexer = new CashScriptLexer(inputStream);
+  const tokenStream = new CommonTokenStream(lexer);
+  const parser = new CashScriptParser(tokenStream);
+  let ast = new AstBuilder(parser.sourceFile()).build() as SourceFileNode;
+  ast = ast.accept(new SymbolTableTraversal()) as SourceFileNode;
+  const traversal = new TypeCheckTraversal();
+
+  return { ast, traversal };
+}
+
+describe('Type Check', () => {
+  describe('Invalid Parameters', () => {
+    const testCases = readCashFiles(path.join(__dirname, 'invalid_parameters'));
+    testCases.forEach((f) => {
+      it(`${f.fn} should throw InvalidParameterTypeError`, () => {
+        const { ast, traversal } = setup(f.contents);
+        assert.throws(() => {
+          ast.accept(traversal);
+        }, InvalidParameterTypeError);
+      });
+    });
+  });
+
+  // describe('Success', () => {
+  //   const testCases = readCashFiles(path.join(__dirname, '..', 'syntax', 'success'));
+  //   testCases.forEach((f) => {
+  //     it(`${f.fn} should succeed`, () => {
+  //       const { ast, traversal } = setup(f.contents);
+  //       assert.doesNotThrow(() => {
+  //         ast.accept(traversal);
+  //       });
+  //     });
+  //   });
+  // });
+});
