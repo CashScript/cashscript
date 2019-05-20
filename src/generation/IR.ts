59548b4f26008ed233b802d92271a2b1c97ff67f
Rosco Kalis <roscokalis@gmail.com>
2019-05-20 16:14:26 +0900

    Make operator opcode compilation more compact
    - Use a mapping object instead of switch case

--- src/generation/IR.ts
+++ src/generation/IR.ts
@@ -80,14 +80,12 @@ export class Replace extends IntermediateOp {
 
 export class toIrOps {
   static fromTimeOp(op: TimeOp): Op[] {
-    switch (op) {
-      case TimeOp.CHECK_LOCKTIME:
-        return [Op.CHECKLOCKTIMEVERIFY, Op.DROP];
-      case TimeOp.CHECK_SEQUENCE:
-        return [Op.CHECKSEQUENCEVERIFY, Op.DROP];
-      default:
-        throw new Error();
-    }
+    const mapping = {
+      [TimeOp.CHECK_LOCKTIME]: [Op.CHECKLOCKTIMEVERIFY, Op.DROP],
+      [TimeOp.CHECK_SEQUENCE]: [Op.CHECKSEQUENCEVERIFY, Op.DROP],
+    };
+
+    return mapping[op];
   }
 
   static fromCast(from: PrimitiveType, to: PrimitiveType): IrOp[] {
@@ -101,78 +99,61 @@ export class toIrOps {
   }
 
   static fromFunction(fn: GlobalFunction): Op[] {
-    switch (fn) {
-      case GlobalFunction.ABS:
-        return [Op.ABS];
-      case GlobalFunction.CHECKDATASIG:
-        return [Op.CHECKDATASIG];
-      case GlobalFunction.CHECKMULTISIG:
-        return [Op.CHECKMULTISIG];
-      case GlobalFunction.CHECKSIG:
-        return [Op.CHECKSIG];
-      case GlobalFunction.MAX:
-        return [Op.MAX];
-      case GlobalFunction.MIN:
-        return [Op.MIN];
-      case GlobalFunction.REQUIRE:
-        return [Op.VERIFY];
-      case GlobalFunction.RIPEMD160:
-        return [Op.RIPEMD160];
-      case GlobalFunction.SHA1:
-        return [Op.SHA1];
-      case GlobalFunction.SHA256:
-        return [Op.SHA256];
-      case GlobalFunction.WITHIN:
-        return [Op.WITHIN];
-      default:
-        throw new Error();
-    }
+    const mapping = {
+      [GlobalFunction.ABS]: [Op.ABS],
+      [GlobalFunction.CHECKDATASIG]: [Op.CHECKDATASIG],
+      [GlobalFunction.CHECKMULTISIG]: [Op.CHECKMULTISIG],
+      [GlobalFunction.CHECKSIG]: [Op.CHECKSIG],
+      [GlobalFunction.MAX]: [Op.MAX],
+      [GlobalFunction.MIN]: [Op.MIN],
+      [GlobalFunction.REQUIRE]: [Op.VERIFY],
+      [GlobalFunction.RIPEMD160]: [Op.RIPEMD160],
+      [GlobalFunction.SHA1]: [Op.SHA1],
+      [GlobalFunction.SHA256]: [Op.SHA256],
+      [GlobalFunction.WITHIN]: [Op.WITHIN],
+    };
+
+    return mapping[fn];
   }
 
   static fromBinaryOp(op: BinaryOperator, numeric: boolean = false): Op[] {
-    switch (op) {
-      case BinaryOperator.DIV:
-        return [Op.DIV];
-      case BinaryOperator.MOD:
-        return [Op.MOD];
-      case BinaryOperator.PLUS:
-        if (numeric) return [Op.ADD];
-        else return [Op.CAT];
-      case BinaryOperator.MINUS:
-        return [Op.SUB];
-      case BinaryOperator.LT:
-        return [Op.LESSTHAN];
-      case BinaryOperator.LE:
-        return [Op.LESSTHANOREQUAL];
-      case BinaryOperator.GT:
-        return [Op.GREATERTHAN];
-      case BinaryOperator.GE:
-        return [Op.GREATERTHANOREQUAL];
-      case BinaryOperator.EQ:
-        if (numeric) return [Op.NUMEQUAL];
-        else return [Op.EQUAL];
-      case BinaryOperator.NE:
-        if (numeric) return [Op.NUMNOTEQUAL];
-        else return [Op.EQUAL, Op.NOT];
-      case BinaryOperator.AND:
-        return [Op.BOOLAND];
-      case BinaryOperator.OR:
-        return [Op.BOOLOR];
-      default:
-        throw new Error();
+    const mapping = {
+      [BinaryOperator.DIV]: [Op.DIV],
+      [BinaryOperator.MOD]: [Op.MOD],
+      [BinaryOperator.PLUS]: [Op.CAT],
+      [BinaryOperator.MINUS]: [Op.SUB],
+      [BinaryOperator.LT]: [Op.LESSTHAN],
+      [BinaryOperator.LE]: [Op.LESSTHANOREQUAL],
+      [BinaryOperator.GT]: [Op.GREATERTHAN],
+      [BinaryOperator.GE]: [Op.GREATERTHANOREQUAL],
+      [BinaryOperator.EQ]: [Op.EQUAL],
+      [BinaryOperator.NE]: [Op.EQUAL, Op.NOT],
+      [BinaryOperator.AND]: [Op.BOOLAND],
+      [BinaryOperator.OR]: [Op.BOOLOR],
+    };
+
+    if (numeric) {
+      switch (op) {
+        case BinaryOperator.PLUS:
+          return [Op.ADD];
+        case BinaryOperator.EQ:
+          return [Op.NUMEQUAL];
+        case BinaryOperator.NE:
+          return [Op.NUMNOTEQUAL];
+        default:
+          return mapping[op];
+      }
     }
+    return mapping[op];
   }
 
   static fromUnaryOp(op: UnaryOperator): Op[] {
-    switch (op) {
-      case UnaryOperator.NOT:
-        return [Op.NOT];
-      case UnaryOperator.NEGATE:
-        return [Op.NEGATE];
-      case UnaryOperator.PLUS:
-        return [];
-      default:
-        throw new Error();
-    }
+    const mapping = {
+      [UnaryOperator.NOT]: [Op.NOT],
+      [UnaryOperator.NEGATE]: [Op.NEGATE],
+      [UnaryOperator.PLUS]: [],
+    };
+
+    return mapping[op];
   }
 }
