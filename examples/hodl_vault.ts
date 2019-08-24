9a256a37ffdc53ff17864e61384033ed2ee79d32
Rosco Kalis <roscokalis@gmail.com>
2019-08-24 13:14:02 +0200

    Add checkdatasig example
    - HODL Vault uses a price oracle that publishes prices each block
    - When this price is greater than a certain target, the money is
    spendable

---
+++ examples/hodl_vault.ts
@@ -0,0 +1,57 @@
+import { BITBOX, Script, Crypto } from 'bitbox-sdk';
+import { TxnDetailsResult } from 'bitcoin-com-rest';
+import { ECPair, HDNode } from 'bitcoincashjs-lib';
+import { Contract, Sig } from 'cashscript';
+import * as path from 'path';
+
+class PriceOracle {
+  constructor(public keypair: ECPair) {}
+
+  // Encode a blockHeight and bchUsdPrice into a byte sequence of 8 bytes (4 bytes per value)
+  createMessage(blockHeight: number, bchUsdPrice: number): Buffer {
+    const lhs = Buffer.alloc(4, 0);
+    const rhs = Buffer.alloc(4, 0);
+    new Script().encodeNumber(blockHeight).copy(lhs);
+    new Script().encodeNumber(bchUsdPrice).copy(rhs);
+    return Buffer.concat([lhs, rhs]);
+  }
+
+  signMessage(message: Buffer): Buffer {
+    return this.keypair.sign(new Crypto().sha256(message)).toDER();
+  }
+}
+
+run();
+export async function run(): Promise<void> {
+  // Initialise BITBOX
+  const network: string = 'testnet';
+  const bitbox: BITBOX = new BITBOX({ restURL: 'https://trest.bitcoin.com/v2/' });
+
+  // Initialise HD node and owner's keypair
+  const rootSeed: Buffer = bitbox.Mnemonic.toSeed('CashScript');
+  const hdNode: HDNode = bitbox.HDNode.fromSeed(rootSeed, network);
+  const owner: ECPair = bitbox.HDNode.toKeyPair(bitbox.HDNode.derive(hdNode, 0));
+
+  // Initialise price oracle with a keypair
+  const oracle = new PriceOracle(bitbox.HDNode.toKeyPair(bitbox.HDNode.derive(hdNode, 1)));
+
+  // Compile and instantiate HODL Vault
+  const HodlVault = Contract.fromCashFile(path.join(__dirname, 'hodl_vault.cash'), 'testnet');
+  const instance = HodlVault.new(
+    bitbox.ECPair.toPublicKey(owner),
+    bitbox.ECPair.toPublicKey(oracle.keypair),
+    597000,
+    30000,
+  );
+
+  // Produce new oracle message and signature
+  const oracleMessage = oracle.createMessage(597000, 30000);
+  const oracleSignature = oracle.signMessage(oracleMessage);
+
+  // Spend from the vault
+  const tx: TxnDetailsResult = await instance.functions
+    .spend(new Sig(owner, 0x01), oracleSignature, oracleMessage)
+    .send(instance.address, 1000);
+
+  console.log(tx);
+}
