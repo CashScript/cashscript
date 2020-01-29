7b564a43518f7d3a4abf6c4015d3173bd507be84
Rosco Kalis <roscokalis@gmail.com>
2020-01-29 12:28:44 +0100

    Add --hex and --asm options to CLI
    - --hex outputs the compiled bytecode hex
    - --asm outputs the compiled bytecode ASM

--- packages/cashc/src/cashc-cli.ts
+++ packages/cashc/src/cashc-cli.ts
@@ -2,6 +2,7 @@
 import yargs from 'yargs';
 import fs from 'fs';
 import path from 'path';
+import { Script as BScript } from 'bitbox-sdk';
 import { CashCompiler, Artifacts, version } from '.';
 import { countOpcodes, Data, calculateBytesize } from './util';
 
@@ -12,39 +13,66 @@ const { argv } = yargs
     describe: 'Specify a file to output the generated artifact.',
     type: 'string',
   })
+  .option('hex', {
+    alias: 'h',
+    describe: 'Compile the contract to hex format rather than a full artifact',
+    type: 'boolean',
+  })
+  .option('asm', {
+    alias: 'A',
+    describe: 'Compile the contract to ASM format rather than a full artifact',
+    type: 'boolean',
+  })
   .showHelpOnFail(true)
   .help()
   .version(version);
 
-ensure(argv._.length === 1, 'Please provide exactly one source file');
-const sourceFile = path.resolve(argv._[0]);
-const outputFile = argv.output && argv.output !== '-' && path.resolve(argv.output);
-ensure(fs.existsSync(sourceFile) && fs.statSync(sourceFile).isFile(), 'Please provide a valid source file');
+run();
 
-try {
-  const artifact = CashCompiler.compileFile(sourceFile);
+function run(): void {
+  ensure(argv._.length === 1, 'Please provide exactly one source file');
+  ensure(!(argv.asm && argv.hex), 'Flags --asm and --hex can not be used together');
+  ensure(!((argv.asm || argv.hex) && argv.output), 'Flags --asm or --hex can not be used with --output');
 
-  const script = Data.asmToScript(artifact.bytecode);
-  const opcount = countOpcodes(script);
-  const bytesize = calculateBytesize(script);
-  if (opcount > 201) {
-    console.warn('Warning: Your contract\'s opcount is over the limit of 201 and will not be accepted by the BCH network');
-  }
-  if (bytesize > 520) {
-    console.warn('Warning: Your contract\'s bytesize is over the limit of 520 and will not be accepted by the BCH network');
-  }
+  const sourceFile = path.resolve(argv._[0]);
+  const outputFile = argv.output && argv.output !== '-' && path.resolve(argv.output);
+  ensure(fs.existsSync(sourceFile) && fs.statSync(sourceFile).isFile(), 'Please provide a valid source file');
 
-  if (!outputFile) {
-    console.log(JSON.stringify(artifact, null, 2));
-  } else {
-    const outputDir = path.dirname(outputFile);
-    if (!fs.existsSync(outputDir)) {
-      fs.mkdirSync(outputDir, { recursive: true });
+  try {
+    const artifact = CashCompiler.compileFile(sourceFile);
+    const script = Data.asmToScript(artifact.bytecode);
+    const opcount = countOpcodes(script);
+    const bytesize = calculateBytesize(script);
+
+    if (opcount > 201) {
+      console.warn('Warning: Your contract\'s opcount is over the limit of 201 and will not be accepted by the BCH network');
     }
-    Artifacts.export(artifact, outputFile);
+    if (bytesize > 520) {
+      console.warn('Warning: Your contract\'s bytesize is over the limit of 520 and will not be accepted by the BCH network');
+    }
+
+    if (argv.asm) {
+      console.log(artifact.bytecode);
+      return;
+    }
+
+    if (argv.hex) {
+      console.log(new BScript().encode(script).toString('hex'));
+      return;
+    }
+
+    if (!outputFile) {
+      console.log(JSON.stringify(artifact, null, 2));
+    } else {
+      const outputDir = path.dirname(outputFile);
+      if (!fs.existsSync(outputDir)) {
+        fs.mkdirSync(outputDir, { recursive: true });
+      }
+      Artifacts.export(artifact, outputFile);
+    }
+  } catch (e) {
+    abort(e.message);
   }
-} catch (e) {
-  abort(e.message);
 }
 
 function ensure(condition: boolean, msg: string, code?: number): void {
