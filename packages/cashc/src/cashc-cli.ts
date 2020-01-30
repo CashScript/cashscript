8fe1e008a4b95308d5dc661b2190b259baa142d7
Rosco Kalis <roscokalis@gmail.com>
2020-01-30 12:55:15 +0100

    Add --args option to CashScript CLI
    - When using --args you can pass in any compiler arguments that are
      prepended to the generated bytecode

--- packages/cashc/src/cashc-cli.ts
+++ packages/cashc/src/cashc-cli.ts
@@ -7,6 +7,7 @@ import { CashCompiler, Artifacts, version } from '.';
 import { countOpcodes, Data, calculateBytesize } from './util';
 
 const { argv } = yargs
+  .parserConfiguration({ 'parse-numbers': false })
   .usage('Usage: $0 [options] [source_file]')
   .option('output', {
     alias: 'o',
@@ -23,6 +24,14 @@ const { argv } = yargs
     describe: 'Compile the contract to ASM format rather than a full artifact',
     type: 'boolean',
   })
+  .option('args', {
+    alias: 'a',
+    describe: 'List of constructor arguments to pass into the contract. '
+      + 'Can only be used in combination with either the --hex or --asm flags. '
+      + 'When compiling to a JSON artifact, contract instantiation should be done through the CashScript SDK. '
+      + 'Note that NO type checking is performed by the cashc CLI, so it is safer to use the CashScript SDK.',
+    type: 'array',
+  })
   .showHelpOnFail(true)
   .help()
   .version(version);
@@ -32,7 +41,8 @@ run();
 function run(): void {
   ensure(argv._.length === 1, 'Please provide exactly one source file');
   ensure(!(argv.asm && argv.hex), 'Flags --asm and --hex can not be used together');
-  ensure(!((argv.asm || argv.hex) && argv.output), 'Flags --asm or --hex can not be used with --output');
+  ensure(!(argv.asm || argv.hex) || !argv.output, 'Flags --asm or --hex can not be used with --output');
+  ensure(argv.asm || argv.hex || !argv.args, '--args can only be used with --asm or --hex');
 
   const sourceFile = path.resolve(argv._[0]);
   const outputFile = argv.output && argv.output !== '-' && path.resolve(argv.output);
@@ -41,6 +51,24 @@ function run(): void {
   try {
     const artifact = CashCompiler.compileFile(sourceFile);
     const script = Data.asmToScript(artifact.bytecode);
+
+    if (argv.args) {
+      argv.args.forEach((arg) => {
+        if (typeof arg !== 'string') return; // Yargs parses everything as string
+        if (arg === 'true') {
+          script.unshift(Data.encodeBool(true));
+        } else if (arg === 'false') {
+          script.unshift(Data.encodeBool(false));
+        } else if (arg.startsWith('0x')) {
+          script.unshift(Buffer.from(arg.substring(2), 'hex'));
+        } else if (!Number.isNaN(Number(arg))) {
+          script.unshift(Data.encodeInt(Number(arg)));
+        } else {
+          script.unshift(Data.encodeString(arg));
+        }
+      });
+    }
+
     const opcount = countOpcodes(script);
     const bytesize = calculateBytesize(script);
 
@@ -52,7 +80,7 @@ function run(): void {
     }
 
     if (argv.asm) {
-      console.log(artifact.bytecode);
+      console.log(Data.scriptToAsm(script));
       return;
     }
 
@@ -81,5 +109,7 @@ function ensure(condition: boolean, msg: string, code?: number): void {
 
 function abort(msg: string, code: number = 1): void {
   console.error(msg);
+  console.error();
+  yargs.showHelp();
   process.exit(code);
 }
