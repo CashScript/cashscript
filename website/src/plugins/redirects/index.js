f9f194539fc4daba2e31d10348276285152af30e
Rosco Kalis <roscokalis@gmail.com>
2020-05-27 13:32:12 +0200

    [Website] Edit redirects

--- website/src/plugins/redirects/index.js
+++ website/src/plugins/redirects/index.js
@@ -1,14 +1,14 @@
 const { docuHash } = require('@docusaurus/utils');
 
 const redirects = [
-  ['/docs/', '/docs/basics/about/'],
-  ['/docs/about/', '/docs/basics/about/'],
-  ['/docs/basics/', '/docs/basics/about/'],
-  ['/docs/language/', '/docs/language/contracts/'],
-  ['/docs/sdk/', '/docs/sdk/instantiation/'],
-  ['/docs/guides/', '/docs/guides/covenants/'],
-  ['/docs/getting-started/', '/docs/basics/getting-started/'],
-  ['/docs/examples/', '/docs/language/examples/'],
+  ['/docs', '/docs/basics/about'],
+  ['/docs/about', '/docs/basics/about'],
+  ['/docs/basics', '/docs/basics/about'],
+  ['/docs/language', '/docs/language/contracts'],
+  ['/docs/sdk', '/docs/sdk/instantiation'],
+  ['/docs/guides', '/docs/guides/covenants'],
+  ['/docs/getting-started', '/docs/basics/getting-started'],
+  ['/docs/examples', '/docs/language/examples'],
 ];
 
 module.exports = function () {
