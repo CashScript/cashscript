9d7185cde230088bd75afdb97423e12d9951e4d9
Rosco Kalis <roscokalis@gmail.com>
2021-10-13 11:31:16 +0200

    Make BigInt tests more descriptive

--- packages/cashscript/test/e2e/staging/BigInt.test.ts
+++ packages/cashscript/test/e2e/staging/BigInt.test.ts
@@ -9,6 +9,8 @@ import { getTxOutputs } from '../../test-util';
 
 describe('BigInt', () => {
   let bigintContract: Contract;
+  const MAX_INT32 = BigInt('2147483647');
+  const MAX_INT64 = BigInt('9223372036854775807');
 
   beforeAll(() => {
     // eslint-disable-next-line global-require
@@ -19,14 +21,14 @@ describe('BigInt', () => {
   });
 
   describe('proofOfBigInt', () => {
-    it('should fail when providing a too small numbers', async () => {
+    it('should fail when providing a number that fits within 32 bits', async () => {
       // given
       const to = bigintContract.address;
       const amount = 1000;
 
       // when
       const txPromise = bigintContract.functions
-        .proofOfBigInt(BigInt('2147483640'), 10)
+        .proofOfBigInt(MAX_INT32, 10)
         .to(to, amount)
         .send();
 
@@ -35,14 +37,14 @@ describe('BigInt', () => {
       await expect(txPromise).rejects.toThrow(Reason.VERIFY);
     });
 
-    it('should fail when providing a too large numbers (but still within the 64bit limit)', async () => {
+    it('should fail when providing numbers that overflow 64 bits when multiplied', async () => {
       // given
       const to = bigintContract.address;
       const amount = 1000;
 
       // when
       const txPromise = bigintContract.functions
-        .proofOfBigInt(BigInt('9223372036854775800'), 10)
+        .proofOfBigInt(MAX_INT64 / BigInt(9), 10)
         .to(to, amount)
         .send();
 
@@ -51,14 +53,14 @@ describe('BigInt', () => {
       await expect(txPromise).rejects.toThrow(Reason.INVALID_NUMBER_RANGE);
     });
 
-    it('should fail when providing too large numbers (outside the 64bit limit)', async () => {
+    it('should fail when providing a number that does not fit within 64 bits', async () => {
       // given
       const to = bigintContract.address;
       const amount = 1000;
 
       // when
       const txPromise = bigintContract.functions
-        .proofOfBigInt(BigInt('9223372036854775808'), 10)
+        .proofOfBigInt(MAX_INT64 + BigInt(1), 10)
         .to(to, amount)
         .send();
 
@@ -72,14 +74,14 @@ describe('BigInt', () => {
       // await expect(txPromise).rejects.toThrow(Reason.INVALID_NUMBER_RANGE);
     });
 
-    it('should succeed when providing the right numbers', async () => {
+    it('should succeed when providing a number within 32b < x < 64b', async () => {
       // given
       const to = bigintContract.address;
       const amount = 1000;
 
       // when
       const tx = await bigintContract.functions
-        .proofOfBigInt(BigInt('2147483648'), 10)
+        .proofOfBigInt(MAX_INT32 + BigInt(1), 10)
         .to(to, amount)
         .send();
 
