825f31904763fa5fb3461ff0bf23027a824afdb4
nathanielCherian <ngcherian201@gmail.com>
2022-05-12 11:54:58 -0700

    automatic versioning

---
+++ bump_version.py
@@ -0,0 +1,30 @@
+import json, sys, os
+
+package_files = [
+    'packages/cashc/package.json',
+    'packages/utils/package.json',
+    'packages/cashscript/package.json',
+]
+
+index_file = 'packages/cashc/src/index.ts'
+
+editing = lambda filename: print('editing %s...' % filename)
+# packages/cashc/src/index.ts
+if __name__ == "__main__":
+    _, version = sys.argv
+    for file in package_files: 
+        with open(file, 'r+') as f:
+            data = json.load(f)
+            data['version'] = str(version)
+            f.seek(0)
+            json.dump(data, f, indent=4)
+            f.truncate()
+        editing(file)
+    
+    with open(index_file, 'r+') as f:
+        data = f.readlines()
+        data[0] = "export const version = '%s'; // Need to keep version on line 1\n" % version
+        f.seek(0)
+        f.write(''.join(data))
+        editing(index_file)
+    print("Changed version to '%s'." % version)
--- packages/cashc/src/index.ts
+++ packages/cashc/src/index.ts
@@ -1,5 +1,6 @@
+export const version = '0.6.5'; // Need to keep version on line 1
+
 export * from './Errors';
 export * as utils from '@cashscript/utils';
 export { compileFile, compileString } from './compiler';
 
-export const version = '0.6.5';
