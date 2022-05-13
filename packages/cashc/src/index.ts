435ced479773ba48906767fc1964b32283cac764
nathanielCherian <ngcherian201@gmail.com>
2022-05-13 14:42:16 -0700

    automatic versioning through ts

--- packages/cashc/src/index.ts
+++ packages/cashc/src/index.ts
@@ -1,5 +1,4 @@
+export const version = '0.6.5'; // keep this on line 1
 export * from './Errors';
 export * as utils from '@cashscript/utils';
 export { compileFile, compileString } from './compiler';
-
-export const version = '0.6.5';
