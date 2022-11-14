b846b937730d0cfc9687c60c23e670b972a8b52f
Rosco Kalis <roscokalis@gmail.com>
2022-11-14 17:12:10 +0100

    Bump version to 0.7.3

--- packages/cashc/src/index.ts
+++ packages/cashc/src/index.ts
@@ -2,4 +2,4 @@ export * from './Errors.js';
 export * as utils from '@cashscript/utils';
 export { compileFile, compileString } from './compiler.js';
 
-export const version = '0.7.2';
+export const version = '0.7.3';
