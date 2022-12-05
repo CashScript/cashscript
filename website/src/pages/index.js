2e7e403ddf10b983ec32057cc5f27088c9d8d16d
mr-zwets <53938059+mr-zwets@users.noreply.github.com>
2022-12-05 21:34:31 +0100

    Add support to CashTokens + P2SH32 to the CashScript compiler (#134)
    - Add tokenCategory, tokenAmount and nftCommitment fields to inputs and outputs
    - Add LockingBytecodeP2SH32
    - Remove deprecated 'testnet' and 'staging' networks

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -53,7 +53,7 @@ const features = [
         <p>
           Most DeFi applications are currently running on top of Ethereum. Bitcoin
           Cash contracts use a different and functionally more limited paradigm.
-          But with techniques such as covenants, state simulation and trust-minimised
+          But with techniques such as covenants, local state and trust-minimised
           price oracles, DeFi is very much possible on Bitcoin Cash.
         </p>
       </>
