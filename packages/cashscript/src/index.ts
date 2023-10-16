7dfdca7d0ca8f070dab3ecec070eb494932feae3
Rosco Kalis <roscokalis@gmail.com>
2023-10-16 17:43:42 -0400

    Export TransactionBuilder -_-

--- packages/cashscript/src/index.ts
+++ packages/cashscript/src/index.ts
@@ -3,6 +3,7 @@ import SignatureTemplate from './SignatureTemplate.js';
 export { SignatureTemplate };
 export { Contract, ContractFunction } from './Contract.js';
 export { Transaction } from './Transaction.js';
+export { TransactionBuilder } from './TransactionBuilder.js';
 export { Argument, encodeArgument } from './Argument.js';
 export { Artifact, AbiFunction, AbiInput } from '@cashscript/utils';
 export * as utils from '@cashscript/utils';
