8aa8a88fb7ddd6c487565515c4b78e350dacd520
Rosco Kalis <roscokalis@gmail.com>
2023-11-01 10:26:28 +0100

    Export * from cashscript interfaces.ts

--- packages/cashscript/src/index.ts
+++ packages/cashscript/src/index.ts
@@ -7,14 +7,7 @@ export { TransactionBuilder } from './TransactionBuilder.js';
 export { Argument, encodeArgument } from './Argument.js';
 export { Artifact, AbiFunction, AbiInput } from '@cashscript/utils';
 export * as utils from '@cashscript/utils';
-export {
-  Utxo,
-  Recipient,
-  SignatureAlgorithm,
-  HashType,
-  Network,
-  isUtxoP2PKH,
-} from './interfaces.js';
+export * from './interfaces.js';
 export * from './Errors.js';
 export {
   NetworkProvider,
