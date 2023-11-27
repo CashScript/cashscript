e8c3a34bfa70fbe725e220188ab45873dfe8a84c
mainnet-pat <mainnet_pat@protonmail.com>
2023-11-27 10:27:29 +0000

    Small cleanups

--- demo/testing-suite/test/example.test.ts
+++ demo/testing-suite/test/example.test.ts
@@ -4,12 +4,10 @@ import { randomUtxo } from 'cashscript/dist/src/utils';
 import 'cashscript/dist/test/JestExtensions';
 
 describe("test example contract functions", () => {
-  it("should check for output logs", async () => {
+  it("should check for output logs and error messages", async () => {
     const provider = new MockNetworkProvider();
     const contract = new Contract(artifact, [], { provider });
     provider.addUtxo(contract.address, randomUtxo());
-
-    artifact.debug.logs[0].ip = 100;
 
     let transaction = contract.functions.test(0n).to(contract.address, 10000n);
     await (expect(transaction)).toLog(/0 test/);
