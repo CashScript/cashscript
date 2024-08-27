4446fc1691b05d2bd50df2189ac75be49f32886f
Rosco Kalis <roscokalis@gmail.com>
2024-08-27 10:55:24 +0200

    Mark some re-exports as 'type' so tsx doesn't complain + update example pragmas

--- packages/cashscript/src/index.ts
+++ packages/cashscript/src/index.ts
@@ -1,16 +1,14 @@
-import SignatureTemplate from './SignatureTemplate.js';
-
-export { SignatureTemplate };
-export { Contract, ContractFunction } from './Contract.js';
+export { default as SignatureTemplate } from './SignatureTemplate.js';
+export { Contract, type ContractFunction } from './Contract.js';
 export { Transaction } from './Transaction.js';
 export { TransactionBuilder } from './TransactionBuilder.js';
-export { Argument, encodeArgument } from './Argument.js';
-export { Artifact, AbiFunction, AbiInput } from '@cashscript/utils';
+export { type Argument, encodeArgument } from './Argument.js';
+export type { Artifact, AbiFunction, AbiInput } from '@cashscript/utils';
 export * as utils from '@cashscript/utils';
 export * from './interfaces.js';
 export * from './Errors.js';
 export {
-  NetworkProvider,
+  type NetworkProvider,
   BitcoinRpcNetworkProvider,
   ElectrumNetworkProvider,
   FullStackNetworkProvider,
--- packages/cashscript/src/network/index.ts
+++ packages/cashscript/src/network/index.ts
@@ -1,4 +1,4 @@
-export { default as NetworkProvider } from './NetworkProvider.js';
+export type { default as NetworkProvider } from './NetworkProvider.js';
 export { default as BitcoinRpcNetworkProvider } from './BitcoinRpcNetworkProvider.js';
 export { default as ElectrumNetworkProvider } from './ElectrumNetworkProvider.js';
 export { default as FullStackNetworkProvider } from './FullStackNetworkProvider.js';
