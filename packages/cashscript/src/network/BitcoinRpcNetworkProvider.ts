e6ec5b3438b2b5591369619ac9f0fa9ed82fff5d
Mathieu <mr-zwets@protonmail.com>
2024-10-28 15:10:47 +0100

    switch to @mr-zwets/bchn-api-wrapper

--- packages/cashscript/src/network/BitcoinRpcNetworkProvider.ts
+++ packages/cashscript/src/network/BitcoinRpcNetworkProvider.ts
@@ -1,20 +1,29 @@
-import RpcClientRetry from 'bitcoin-rpc-promise-retry';
 import { Utxo, Network } from '../interfaces.js';
 import NetworkProvider from './NetworkProvider.js';
+import {
+  BchnRpcClient,
+  type GetBlockCount,
+  type GetRawTransactionVerbosity0,
+  type ListUnspent,
+  type SendRawTransaction,
+} from '@mr-zwets/bchn-api-wrapper';
 
 export default class BitcoinRpcNetworkProvider implements NetworkProvider {
-  private rpcClient: IRpcClientRetry;
+  private rpcClient: BchnRpcClient;
 
   constructor(
     public network: Network,
     url: string,
-    opts?: object,
+    opts: {
+      rpcUser: string;
+      rpcPassword: string;
+    },
   ) {
-    this.rpcClient = new RpcClientRetry(url, opts);
+    this.rpcClient = new BchnRpcClient({ url, ...opts });
   }
 
   async getUtxos(address: string): Promise<Utxo[]> {
-    const result = await this.rpcClient.listUnspent(0, 9999999, [address]);
+    const result = await this.rpcClient.request<ListUnspent>('listunspent', 0, 9999999, [address]);
 
     const utxos = result.map((utxo) => ({
       txid: utxo.txid,
@@ -26,55 +35,18 @@ export default class BitcoinRpcNetworkProvider implements NetworkProvider {
   }
 
   async getBlockHeight(): Promise<number> {
-    return this.rpcClient.getBlockCount();
+    return this.rpcClient.request<GetBlockCount>('getblockcount');
   }
 
   async getRawTransaction(txid: string): Promise<string> {
-    return this.rpcClient.getRawTransaction(txid);
+    return this.rpcClient.request<GetRawTransactionVerbosity0>('getrawtransaction', txid, 0);
   }
 
   async sendRawTransaction(txHex: string): Promise<string> {
-    return this.rpcClient.sendRawTransaction(txHex);
+    return this.rpcClient.request<SendRawTransaction>('sendrawtransaction', txHex);
   }
 
-  getClient(): IRpcClientRetry {
+  getClient(): BchnRpcClient {
     return this.rpcClient;
   }
-}
-
-interface ListUnspentItem {
-  txid: string;
-  vout: number;
-  address: string;
-  label: string;
-  scriptPubKey: string;
-  amount: number;
-  confirmations: number;
-  redeemScript: string;
-  spendable: boolean;
-  solvable: boolean;
-  safe: boolean;
-}
-
-interface IRpcClientRetry {
-  constructor(url: string, opts?: object): void;
-  listUnspent(
-    minConf?: number,
-    maxConf?: number,
-    addresses?: string[],
-    includeUnsafe?: boolean,
-    queryOptions?: object,
-  ): Promise<ListUnspentItem[]>;
-  getBlockCount(): Promise<number>;
-  getRawTransaction(txid: string, verbose?: boolean, blockHash?: string): Promise<string>;
-  sendRawTransaction(hexString: string, allowHighFees?: boolean): Promise<string>;
-
-  // below are not required for NetworkProvider interface, but very useful
-  generate(nBlocks: number, maxTries?: number): Promise<string[]>;
-  generateToAddress(nBlocks: number, address: string, maxTries?: number): Promise<string[]>;
-  getNewAddress(label?: string): Promise<string>;
-  dumpPrivKey(address: string): Promise<string>;
-  getBalance(dummy?: string, minConf?: number, includeWatchOnly?: boolean): Promise<number>;
-  getBlock(blockHash: string, verbosity?: number): Promise<string>;
-  importAddress(address: string, label?: string, rescan?: boolean, p2sh?: boolean): Promise<void>;
 }
