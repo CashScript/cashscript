ae2acf47922ccc7b9bbb596779e4f7c7000a3a20
Mathieu Geukens <mr-zwets@protonmail.com>
2024-11-02 11:34:12 +0100

    update FullStackNetworkProvider & improve tests

---
+++ packages/cashscript/test/e2e/network/FullStack.test.ts
@@ -0,0 +1,74 @@
+import BCHJS from '@psf/bch-js';
+import { Contract, SignatureTemplate, FullStackNetworkProvider } from '../../../src/index.js';
+import {
+  alicePriv,
+  bobPkh,
+  bobPriv,
+  bobPub,
+} from '../../fixture/vars.js';
+import { getTxOutputs } from '../../test-util.js';
+import { FailedRequireError } from '../../../src/Errors.js';
+import artifact from '../../fixture/p2pkh.json' with { type: 'json' };
+
+describe('test FullStackNetworkProvider', () => {
+  const provider = new FullStackNetworkProvider('mainnet', new BCHJS({ restURL: 'https://api.fullstack.cash/v5/' }));
+
+  describe('get utxos using FullStackNetworkProvider', () => {
+    it('should get the utxos for a p2sh20 contract', async () => {
+      // Note: We instantiate the contract with bobPkh to avoid mempool conflicts with other tests
+      const p2pkhInstance = new Contract(artifact, [bobPkh], { provider, addressType: 'p2sh20' });
+      console.log(p2pkhInstance.address);
+
+      const utxos = await p2pkhInstance.getUtxos();
+      expect(Array.isArray(utxos)).toBe(true);
+    });
+    // Note: does not currently support p2sh32
+    it.skip('should get the utxos for a p2sh32 contract', async () => {
+      // Note: We instantiate the contract with bobPkh to avoid mempool conflicts with other tests
+      const p2pkhInstance = new Contract(artifact, [bobPkh], { provider, addressType: 'p2sh32' });
+      console.log(p2pkhInstance.address);
+
+      const utxos = await p2pkhInstance.getUtxos();
+      expect(Array.isArray(utxos)).toBe(true);
+    });
+  });
+
+  describe('send using FullStackNetworkProvider', () => {
+    // Note: We instantiate the contract with bobPkh to avoid mempool conflicts with other tests
+    const p2pkhInstance = new Contract(artifact, [bobPkh], { provider, addressType: 'p2sh20' });
+    console.log(p2pkhInstance.address);
+
+    it('should fail when using incorrect function arguments', async () => {
+      // given
+      const to = p2pkhInstance.address;
+      const amount = 10000n;
+
+      // when
+      const txPromise = p2pkhInstance.functions
+        .spend(bobPub, new SignatureTemplate(alicePriv))
+        .to(to, amount)
+        .send();
+
+      // then
+      await expect(txPromise).rejects.toThrow(FailedRequireError);
+      await expect(txPromise).rejects.toThrow('P2PKH.cash:5 Require statement failed at input 0 in contract P2PKH.cash at line 5.');
+    });
+
+    it.skip('should succeed when using correct function arguments', async () => {
+      // given
+      const to = p2pkhInstance.address;
+      const amount = 10000n;
+
+      // when
+      const tx = await p2pkhInstance.functions
+        .spend(bobPub, new SignatureTemplate(bobPriv))
+        .to(to, amount)
+        .send();
+
+      // then
+      const txOutputs = getTxOutputs(tx);
+      expect(txOutputs).toEqual(expect.arrayContaining([{ to, amount: amount.toString() }]));
+      expect(tx.txid).toBeDefined();
+    });
+  });
+});
