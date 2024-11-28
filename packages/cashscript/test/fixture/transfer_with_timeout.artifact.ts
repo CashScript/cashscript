4539b221faf9329e7e094e4acb1b63c0733af061
Rosco Kalis <roscokalis@gmail.com>
2024-11-28 10:14:04 +0100

    Add (broken) tests for contract function typings

---
+++ packages/cashscript/test/fixture/transfer_with_timeout.artifact.ts
@@ -0,0 +1,44 @@
+export default {
+  contractName: 'TransferWithTimeout',
+  constructorInputs: [
+    {
+      name: 'sender',
+      type: 'pubkey',
+    },
+    {
+      name: 'recipient',
+      type: 'pubkey',
+    },
+    {
+      name: 'timeout',
+      type: 'int',
+    },
+  ],
+  abi: [
+    {
+      name: 'transfer',
+      inputs: [
+        {
+          name: 'recipientSig',
+          type: 'sig',
+        },
+      ],
+    },
+    {
+      name: 'timeout',
+      inputs: [
+        {
+          name: 'senderSig',
+          type: 'sig',
+        },
+      ],
+    },
+  ],
+  bytecode: 'OP_3 OP_PICK OP_0 OP_NUMEQUAL OP_IF OP_4 OP_ROLL OP_ROT OP_CHECKSIG OP_NIP OP_NIP OP_NIP OP_ELSE OP_3 OP_ROLL OP_1 OP_NUMEQUALVERIFY OP_3 OP_ROLL OP_SWAP OP_CHECKSIGVERIFY OP_SWAP OP_CHECKLOCKTIMEVERIFY OP_2DROP OP_1 OP_ENDIF',
+  source: "contract TransferWithTimeout(\n    pubkey sender,\n    pubkey recipient,\n    int timeout\n) {\n    // Require recipient's signature to match\n    function transfer(sig recipientSig) {\n        require(checkSig(recipientSig, recipient));\n    }\n\n    // Require timeout time to be reached and sender's signature to match\n    function timeout(sig senderSig) {\n        require(checkSig(senderSig, sender));\n        require(tx.time >= timeout);\n    }\n}\n",
+  compiler: {
+    name: 'cashc',
+    version: '0.8.0-next.0',
+  },
+  updatedAt: '2023-02-10T15:22:51.144Z',
+};
--- packages/cashscript/test/types/Contract.types.test.ts
+++ packages/cashscript/test/types/Contract.types.test.ts
@@ -1,64 +1,148 @@
 /* eslint-disable */
-import { Contract } from 'cashscript';
+import { Contract, SignatureTemplate } from 'cashscript';
 import p2pkhArtifact from '../fixture/p2pkh.artifact';
 import p2pkhArtifactJsonNotConst from '../fixture/p2pkh.json';
 import announcementArtifact from '../fixture/announcement.artifact';
 import hodlVaultArtifact from '../fixture/hodl_vault.artifact';
-import { alicePkh, alicePub, oraclePub } from '../fixture/vars';
+import transferWithTimeoutArtifact from '../fixture/transfer_with_timeout.artifact';
+import { alicePkh, alicePriv, alicePub, bobPub, oraclePub } from '../fixture/vars';
 import { binToHex } from '@bitauth/libauth';
 
-// describe('P2PKH contract | single constructor input')
+// describe('P2PKH contract | single constructor input | single function (2 args)')
 {
-  // it('should not give type errors when using correct constructor inputs')
-  new Contract(p2pkhArtifact, [alicePkh]);
-  new Contract(p2pkhArtifact, [binToHex(alicePkh)]);
+  // describe('Contructor arguments')
+  {
+    // it('should not give type errors when using correct constructor inputs')
+    new Contract(p2pkhArtifact, [alicePkh]);
+    new Contract(p2pkhArtifact, [binToHex(alicePkh)]);
 
-  // it('should give type errors when using empty constructor inputs')
-  // @ts-expect-error
-  new Contract(p2pkhArtifact, []);
+    // it('should give type errors when using empty constructor inputs')
+    // @ts-expect-error
+    new Contract(p2pkhArtifact, []);
 
-  // it('should give type errors when using incorrect constructor input type')
-  // @ts-expect-error
-  new Contract(p2pkhArtifact, [1000n]);
+    // it('should give type errors when using incorrect constructor input type')
+    // @ts-expect-error
+    new Contract(p2pkhArtifact, [1000n]);
 
-  // it('should give type errors when using incorrect constructor input length')
-  // @ts-expect-error
-  new Contract(p2pkhArtifact, [alicePkh, 1000n]);
+    // it('should give type errors when using incorrect constructor input length')
+    // @ts-expect-error
+    new Contract(p2pkhArtifact, [alicePkh, 1000n]);
 
-  // it('should not perform type checking when cast to any')
-  new Contract(p2pkhArtifact as any, [alicePkh, 1000n]);
+    // it('should not perform type checking when cast to any')
+    new Contract(p2pkhArtifact as any, [alicePkh, 1000n]);
 
-  // it('should not perform type checking when cannot infer type')
-  // Note: would be very nice if it *could* infer the type from static json
-  new Contract(p2pkhArtifactJsonNotConst, [alicePkh, 1000n]);
+    // it('should not perform type checking when cannot infer type')
+    // Note: would be very nice if it *could* infer the type from static json
+    new Contract(p2pkhArtifactJsonNotConst, [alicePkh, 1000n]);
+  }
+
+  // describe('Contract functions')
+  {
+    const contract = new Contract(p2pkhArtifact, [alicePkh]);
+
+    // it('should not give type errors when using correct function inputs')
+    contract.functions.spend(alicePub, new SignatureTemplate(alicePriv));
+
+    // it('should give type errors when calling a function that does not exist')
+    // @ts-expect-error
+    contract.functions.notAFunction();
+
+    // it('should give type errors when using incorrect function input types')
+    // @ts-expect-error
+    contract.functions.spend(1000n, true);
+
+    // it('should give type errors when using incorrect function input length')
+    // @ts-expect-error
+    contract.functions.spend(alicePub, new SignatureTemplate(alicePriv), 100n);
+    // @ts-expect-error
+    contract.functions.spend(alicePub);
+  }
 }
 
-// describe('Announcement contract | no constructor inputs')
+// describe('Announcement contract | no constructor inputs | single function (no args)')
 {
-  // it('should not give type errors when using correct constructor inputs')
-  new Contract(announcementArtifact, []);
+  // describe('Constructor arguments')
+  {
+    // it('should not give type errors when using correct constructor inputs')
+    new Contract(announcementArtifact, []);
 
-  // it('should give type errors when using incorrect constructor input length')
-  // @ts-expect-error
-  new Contract(announcementArtifact, [1000n]);
+    // it('should give type errors when using incorrect constructor input length')
+    // @ts-expect-error
+    new Contract(announcementArtifact, [1000n]);
 
-  // it('should give type errors when passing in completely incorrect type')
-  // @ts-expect-error
-  new Contract(announcementArtifact, 'hello');
+    // it('should give type errors when passing in completely incorrect type')
+    // @ts-expect-error
+    new Contract(announcementArtifact, 'hello');
+  }
+
+  // describe('Contract functions')
+  {
+    // it('should not give type errors when using correct function inputs')
+    const contract = new Contract(announcementArtifact, []);
+
+    // it('should not give type errors when using correct function inputs')
+    contract.functions.announce();
+
+    // it('should give type errors when calling a function that does not exist')
+    // @ts-expect-error
+    contract.functions.notAFunction();
+
+    // it('should give type errors when using incorrect function input length')
+    // @ts-expect-error
+    contract.functions.announce('hello world');
+  }
 }
 
-// describe('HodlVault contract | 4 constructor inputs')
+// describe('HodlVault contract | 4 constructor inputs | single function (3 args)')
 {
-  // it('should not give type errors when using correct constructor inputs')
-  new Contract(hodlVaultArtifact, [alicePub, binToHex(oraclePub), 1000n, 1000n]);
+  // describe('Constructor arguments')
+  {
+    // it('should not give type errors when using correct constructor inputs')
+    new Contract(hodlVaultArtifact, [alicePub, binToHex(oraclePub), 1000n, 1000n]);
 
-  // it('should give type errors when using too few constructor inputs')
-  // @ts-expect-error
-  new Contract(hodlVaultArtifact, [alicePub, binToHex(oraclePub)]);
+    // it('should give type errors when using too few constructor inputs')
+    // @ts-expect-error
+    new Contract(hodlVaultArtifact, [alicePub, binToHex(oraclePub)]);
 
-  // it('should give type errors when using incorrect constructor input type')
-  // @ts-expect-error
-  new Contract(hodlVaultArtifact, [alicePub, binToHex(oraclePub), 1000n, 'hello']);
-  // @ts-expect-error
-  new Contract(hodlVaultArtifact, [alicePub, binToHex(oraclePub), true, 1000n]);
+    // it('should give type errors when using incorrect constructor input type')
+    // @ts-expect-error
+    new Contract(hodlVaultArtifact, [alicePub, binToHex(oraclePub), 1000n, 'hello']);
+    // @ts-expect-error
+    new Contract(hodlVaultArtifact, [alicePub, binToHex(oraclePub), true, 1000n]);
+  }
+}
+
+
+// describe('TransferWithTimeout contract | 3 constructor inputs | two functions (1 arg each)')
+{
+  // describe('Constructor arguments')
+  {
+    // it('should not give type errors when using correct constructor inputs')
+    new Contract(transferWithTimeoutArtifact, [alicePub, bobPub, 100_000n]);
+  }
+
+  // describe('Contract functions')
+  {
+    const contract = new Contract(transferWithTimeoutArtifact, [alicePub, bobPub, 100_000n]);
+
+    // it('should not give type errors when using correct function inputs')
+    contract.functions.transfer(new SignatureTemplate(alicePriv));
+    contract.functions.timeout(new SignatureTemplate(alicePriv));
+
+    // it('should give type errors when calling a function that does not exist')
+    // @ts-expect-error
+    contract.functions.notAFunction();
+
+    // it('should give type errors when using incorrect function input types')
+    // @ts-expect-error
+    contract.functions.transfer(1000n);
+    // @ts-expect-error
+    contract.functions.timeout('hello');
+
+    // it('should give type errors when using incorrect function input length')
+    // @ts-expect-error
+    contract.functions.transfer(new SignatureTemplate(alicePub), 100n);
+    // @ts-expect-error
+    contract.functions.timeout();
+  }
 }
