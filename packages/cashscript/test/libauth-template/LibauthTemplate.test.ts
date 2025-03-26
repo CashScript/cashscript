050b480f68794a5eb26043c7e19854095c9aad13
Mathieu Geukens <mr-zwets@protonmail.com>
2025-03-26 11:39:13 +0100

    update libauth-template fixtures to use TransactionBuilder

--- packages/cashscript/test/libauth-template/LibauthTemplate.test.ts
+++ packages/cashscript/test/libauth-template/LibauthTemplate.test.ts
@@ -1,11 +1,21 @@
 import { fixtures } from '../fixture/libauth-template/fixtures.js';
+import { fixtures as oldFixtures } from '../fixture/libauth-template/old-fixtures.js';
 
 describe('Libauth Template generation tests', () => {
   fixtures.forEach((fixture) => {
-    it(`should generate a valid libauth template for ${fixture.name}`, async () => {
+    it(`should generate a valid libauth template for ${fixture.name}`, () => {
+      const generatedTemplate = fixture.transaction.getLibauthTemplate();
+      // console.log(JSON.stringify(generatedTemplate, null, 2));
+      // console.log(fixture.transaction.bitauthUri());
+      expect(generatedTemplate).toEqual(fixture.template);
+    });
+  });
+  // old-fixtures using the deprecated simple transaction builder
+  oldFixtures.forEach((fixture) => {
+    it(`should generate a valid libauth template for old-fixture ${fixture.name}`, async () => {
       const generatedTemplate = await fixture.transaction.getLibauthTemplate();
       // console.log(JSON.stringify(generatedTemplate, null, 2));
-      // console.log(await fixture.transaction.bitauthUri());
+      // console.log(fixture.transaction.bitauthUri());
       expect(generatedTemplate).toEqual(fixture.template);
     });
   });
