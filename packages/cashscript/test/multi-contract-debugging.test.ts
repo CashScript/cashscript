8f0a80821d11881e9f780c626323f24bd289c19a
Rosco Kalis <roscokalis@gmail.com>
2025-03-28 11:29:00 +0100

    Add more test cases to multi-contract-debugging tests

--- packages/cashscript/test/multi-contract-debugging.test.ts
+++ packages/cashscript/test/multi-contract-debugging.test.ts
@@ -6,6 +6,7 @@ import {
   TransactionBuilder,
 } from './../src/index.js';
 import {
+  alicePub,
   bobAddress,
   bobPkh,
   bobPriv,
@@ -21,10 +22,70 @@ const provider = new MockNetworkProvider();
 
 describe('Multi-Contract-Debugging tests', () => {
   describe('require statements', () => {
+    it('it should not throw an error if no require statement fails', async () => {
+      const p2pkhContract = new Contract(p2pkhArtifact, [bobPkh], { provider });
+      const bigintContract = new Contract(bigintArtifact, [], { provider });
+
+      const MAX_INT64 = BigInt('9223372036854775807');
+
+      (provider as any).addUtxo?.(p2pkhContract.address, randomUtxo());
+      (provider as any).addUtxo?.(bigintContract.address, randomUtxo());
+      (provider as any).addUtxo?.(bobAddress, randomUtxo());
+
+      const to = p2pkhContract.address;
+      const amount = 10000n;
+      const p2pkhContractUtxos = await p2pkhContract.getUtxos();
+      const bigIntContractUtxos = await bigintContract.getUtxos();
+      const bobAddressUtxos = await provider.getUtxos(bobAddress);
+
+      // when
+      const transaction = new TransactionBuilder({ provider })
+        // wrong public key
+        .addInput(p2pkhContractUtxos[0], p2pkhContract.unlock.spend(bobPub, bobSignatureTemplate))
+        .addInput(bigIntContractUtxos[0], bigintContract.unlock.proofOfBigInt(MAX_INT64 + 1n, 1n))
+        .addInput(bobAddressUtxos[0], bobSignatureTemplate.unlockP2PKH())
+        .addOutput({ to, amount });
+
+      console.warn(transaction.bitauthUri());
+
+      await expect(transaction).toFailRequire();
+      // throw new Error('test');
+    });
+
+    it('it should fail if a require statement fails', async () => {
+      const p2pkhContract = new Contract(p2pkhArtifact, [bobPkh], { provider });
+      const bigintContract = new Contract(bigintArtifact, [], { provider });
+
+      const MAX_INT64 = BigInt('9223372036854775807');
+
+      (provider as any).addUtxo?.(p2pkhContract.address, randomUtxo());
+      (provider as any).addUtxo?.(bigintContract.address, randomUtxo());
+      (provider as any).addUtxo?.(bobAddress, randomUtxo());
+
+      const to = p2pkhContract.address;
+      const amount = 10000n;
+      const p2pkhContractUtxos = await p2pkhContract.getUtxos();
+      const bigIntContractUtxos = await bigintContract.getUtxos();
+      const bobAddressUtxos = await provider.getUtxos(bobAddress);
+
+      // when
+      const transaction = new TransactionBuilder({ provider })
+        // wrong public key
+        .addInput(p2pkhContractUtxos[0], p2pkhContract.unlock.spend(alicePub, bobSignatureTemplate))
+        .addInput(bigIntContractUtxos[0], bigintContract.unlock.proofOfBigInt(MAX_INT64 + 1n, 1n))
+        .addInput(bobAddressUtxos[0], bobSignatureTemplate.unlockP2PKH())
+        .addOutput({ to, amount });
+
+      console.warn(transaction.bitauthUri());
+
+      await expect(transaction).toFailRequire();
+      // throw new Error('test');
+    });
+
     it('it should only fail with correct error message when a final verify fails', async () => {
       const p2pkhContract = new Contract(p2pkhArtifact, [bobPkh], { provider });
       const bigintContract = new Contract(bigintArtifact, [], { provider });
-      
+
       const MAX_INT64 = BigInt('9223372036854775807');
 
       (provider as any).addUtxo?.(p2pkhContract.address, randomUtxo());
@@ -45,7 +106,7 @@ describe('Multi-Contract-Debugging tests', () => {
         .addOutput({ to, amount });
 
       console.warn(transaction.bitauthUri());
-      
+
       await expect(transaction).toFailRequire();
     });
   });
