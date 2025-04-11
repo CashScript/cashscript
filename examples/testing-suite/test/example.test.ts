6fcda2262537be7faf2210a4217809221532b1ff
Rosco Kalis <roscokalis@gmail.com>
2025-04-11 11:10:40 +0200

    Small extra updates from JSON to TS artifacts

--- examples/testing-suite/test/example.test.ts
+++ examples/testing-suite/test/example.test.ts
@@ -1,4 +1,4 @@
-import artifact from '../artifacts/example.json' with { type: 'json' };
+import artifact from '../artifacts/example.artifact.js';
 import { Contract, MockNetworkProvider, TransactionBuilder, randomUtxo } from 'cashscript';
 import 'cashscript/jest';
 
@@ -6,7 +6,7 @@ describe('test example contract functions', () => {
   it('should check for output logs and error messages', async () => {
     const provider = new MockNetworkProvider();
     const contract = new Contract(artifact, [], { provider });
-    
+
     // Create a contract Utxo
     const contractUtxo = randomUtxo();
     provider.addUtxo(contract.address, contractUtxo);
