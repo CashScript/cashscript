48a7918b32eff054dce273692dbeff7aac6bf8bc
mainnet-pat <mainnet_pat@protonmail.com>
2025-07-14 16:07:19 +0000

    Throw on an attempt to submit the same transaction twice

--- packages/cashscript/test/e2e/network/MockNetworkProvider.test.ts
+++ packages/cashscript/test/e2e/network/MockNetworkProvider.test.ts
@@ -30,9 +30,13 @@ describe('Transaction Builder', () => {
     expect(await provider.getUtxos(p2pkhInstance.address)).toHaveLength(0);
 
     // add by address
-    provider.addUtxo(aliceAddress, randomUtxo());
+    provider.addUtxo(aliceAddress, randomUtxo({
+      satoshis: 1100n,
+    }));
     // add by locking bytecode
-    provider.addUtxo(binToHex(addressToLockScript(p2pkhInstance.address)), randomUtxo());
+    provider.addUtxo(binToHex(addressToLockScript(p2pkhInstance.address)), randomUtxo({
+      satoshis: 1100n,
+    }));
 
     const aliceUtxos = await provider.getUtxos(aliceAddress);
     const p2pkhUtxos = await provider.getUtxos(p2pkhInstance.address);
@@ -43,11 +47,18 @@ describe('Transaction Builder', () => {
     const sigTemplate = new SignatureTemplate(alicePriv);
 
     // spend both utxos to bob
-    new TransactionBuilder({provider})
-      .addInput(p2pkhUtxos[0], p2pkhInstance.unlock.spend(alicePub, sigTemplate))
-      .addInput(aliceUtxos[0], sigTemplate.unlockP2PKH())
-      .addOutput({ to: bobAddress, amount: 1000n })
-      .send();
+    const builder = new TransactionBuilder({ provider })
+      .addInputs(p2pkhUtxos, p2pkhInstance.unlock.spend(alicePub, sigTemplate))
+      .addInputs(aliceUtxos, sigTemplate.unlockP2PKH())
+      .addOutput({ to: bobAddress, amount: 2000n });
+
+    const tx = builder.build();
+
+    // try to send invalid transaction
+    await expect(provider.sendRawTransaction(tx.slice(0, -2))).rejects.toThrow('Error reading transaction.');
+
+    // send valid transaction
+    await expect(provider.sendRawTransaction(tx)).resolves.not.toThrow();
 
     // utxos should be removed from the provider
     expect(await provider.getUtxos(aliceAddress)).toHaveLength(0);
@@ -55,5 +66,7 @@ describe('Transaction Builder', () => {
 
     // utxo should be added to bob
     expect(await provider.getUtxos(bobAddress)).toHaveLength(1);
+
+    await expect(provider.sendRawTransaction(tx)).rejects.toThrow('txn-mempool-conflict');
   });
 });
