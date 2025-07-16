8af6f6896a3ae58b5262e9aa532f211623f3299a
Harold Hdz <0x6661df610053ac@gmail.com>
2025-07-16 05:48:38 -0600

    04F1E746CEA02D60EBC98046DE22AA021541CA9E05DB538755C2DEF11D4D5A274FACCD1212002802034B5D7947DCF551619CE30F158DEA751702EEDA0DBB801BF6

--- packages/cashscript/src/SignatureTemplate.ts
+++ packages/cashscript/src/SignatureTemplate.ts
@@ -2,20 +2,20 @@ import { decodePrivateKeyWif, secp256k1, SigningSerializationFlag } from '@bitau
 import { HashType, SignatureAlgorithm } from './interfaces.js';
 
 export default class SignatureTemplate {
-  private privateKey: Uint8Array;
+    private privateKey: Uint8Array;
 
-  constructor(
-    signer: Keypair | Uint8Array | string,
-    private hashtype: HashType = HashType.SIGHASH_ALL | HashType.SIGHASH_UTXOS,
-    private signatureAlgorithm: SignatureAlgorithm = SignatureAlgorithm.SCHNORR,
-  ) {
-    if (isKeypair(signer)) {
-      const wif = signer.toWIF();
-      this.privateKey = decodeWif(wif);
-    } else if (typeof signer === 'string') {
-      this.privateKey = decodeWif(signer);
-    } else {
-      this.privateKey = signer;
+      constructor(
+          signer: Keypair | Uint8Array | string,
+              private hashtype: HashType = HashType.SIGHASH_ALL | HashType.SIGHASH_UTXOS,
+                  private signatureAlgorithm: SignatureAlgorithm = SignatureAlgorithm.SCHNORR,
+                    ) {
+                        if (isKeypair(signer)) {
+                              const wif = signer.toWIF();
+                                    this.privateKey = decodeWif(wif);
+                                        } else if (typeof signer === 'string') {
+                                              this.privateKey = decodeWif(signer);
+                                                  } else {
+                                                        this.privateKey = signer;
     }
   }
 
@@ -32,25 +32,25 @@ export default class SignatureTemplate {
   }
 
   getPublicKey(): Uint8Array {
-    return secp256k1.derivePublicKeyCompressed(this.privateKey) as Uint8Array;
-  }
-}
+                                                               return secp256k1.derivePublicKeyCompressed(this.privateKey) as Uint8Array;
+                                                                 }
+                                                                 }
 
-// Works for both BITBOX/bitcoincash.js ECPair and bitcore-lib-cash PrivateKey
-interface Keypair {
-  toWIF(): string;
-}
+                                                                 // Works for both BITBOX/bitcoincash.js ECPair and bitcore-lib-cash PrivateKey
+                                                                 interface Keypair {
+                                                                   toWIF(): string;
+                                                                   }
 
-function isKeypair(obj: any): obj is Keypair {
-  return typeof obj.toWIF === 'function';
-}
+                                                                   function isKeypair(obj: any): obj is Keypair {
+                                                                     return typeof obj.toWIF === 'function';
+                                                                     }
 
-function decodeWif(wif: string): Uint8Array {
-  const result = decodePrivateKeyWif(wif);
+                                                                     function decodeWif(wif: string): Uint8Array {
+                                                                       const result = decodePrivateKeyWif(wif);
 
-  if (typeof result === 'string') {
-    throw new Error(result);
-  }
+                                                                         if (typeof result === 'string') {
+                                                                             throw new Error(result);
+                                                                               }
 
-  return result.privateKey;
-}
+                                                                                 return result.privateKey;
+                                                                                 }
