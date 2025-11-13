822d566be32c68a8859708b7e01cad8f580b7785
Rosco Kalis <roscokalis@gmail.com>
2025-11-13 14:50:50 +0100

    Fix tests

--- packages/cashscript/test/fixture/debugging/debugging_contracts.ts
+++ packages/cashscript/test/fixture/debugging/debugging_contracts.ts
@@ -264,18 +264,19 @@ contract Test(pubkey owner) {
       require(tx.outputs[this.activeInputIndex].value == inputValue - 1000);
     }
   }
+}
 `;
 
 const CONTRACT_TEST_LOG_INSIDE_LOOP = `
 contract Test() {
   function test_log_inside_loop() {
-    int x = 0;
+    int i = 0;
     do {
-      console.log('x:', x);
-      x = x + 1;
-    } while (x < 10);
+      console.log('i:', i);
+      i = i + 1;
+    } while (i < 10);
 
-    require(x == 10);
+    require(i == 10);
   }
 
   function test_log_inside_loop_complex() {
