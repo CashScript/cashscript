5d8f3cba2067ce272e60702e43c6ad7e717f7bd4
Rosco Kalis <roscokalis@gmail.com>
2026-06-04 10:39:30 +0200

    Export ast/Location in cashc

--- packages/cashc/src/index.ts
+++ packages/cashc/src/index.ts
@@ -1,6 +1,7 @@
 export * from './Errors.js';
 export * as utils from '@cashscript/utils';
 export { compileFile, compileString, type CompileOptions } from './compiler.js';
+export * from './ast/Location.js';
 export * from './ast/error-listeners.js';
 
 export const version = '0.13.0';
