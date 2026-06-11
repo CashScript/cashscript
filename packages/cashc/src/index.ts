2641131edde45a45bfead6d69362055cf561a733
Rosco Kalis <roscokalis@gmail.com>
2026-06-11 10:51:28 +0200

    Bump version to 0.12.2

--- packages/cashc/src/index.ts
+++ packages/cashc/src/index.ts
@@ -3,4 +3,4 @@ export * as utils from '@cashscript/utils';
 export { compileFile, compileString, type CompileOptions } from './compiler.js';
 export * from './ast/error-listeners.js';
 
-export const version = '0.12.1';
+export const version = '0.12.2';
